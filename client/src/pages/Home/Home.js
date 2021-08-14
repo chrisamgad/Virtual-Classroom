72cdc62e355787310c4e6c7c820e562803176998
Chris <chrisamgad@gmail.com>
2021-08-14 21:18:56 +0200

    added slider carousel to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Slider from '../../components/Slider/Slider'
 const Home = () =>{
 
     return(
         <div>
-            Home
+            <Slider/>
         </div>
     )
 }
