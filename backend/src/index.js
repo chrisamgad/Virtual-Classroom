a1e68c5d52d8231c4386da9ac36adb3018461ad4
Chris <chrisamgad@gmail.com>
2021-08-16 15:37:42 +0200

    added parsing of incoming JSON requests to server and configured public folder to be static environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,17 @@ const app = express()
 const studentsRouter = require('./routes/student')
 const teachersRouter = require('./routes/teacher')
 
+
+app.use(express.json()) //express.json() automatically recognizes incoming request object as JSON objects ,then parses any incoming JSON to server into javascript Object so we are able to access these incoming messages such as req.body
+app.use(express.urlencoded({ extended: true })); //Express.urlencoded() expects request data to be sent encoded in the URL, usually in strings or arrays:
+                                                //In other words, handles application/x-www-form-urlencoded
+
+app.use(express.static(__dirname + '/../public'));
 const Student = require ('./Models/student')
 
 const student = new Student({ fullname: 'Chris', email:'chrisamgad@yahoo.com',ID:900170819,password:'123',role:'student' });
 student.save()
+
 
 
 app.use(studentsRouter)
--- backend/src/routes/student.js
+++ backend/src/routes/student.js
@@ -1,10 +1,16 @@
 const express = require('express')
-
+const Student = require('../Models/student')// requiring student model
 var router = express.Router()
 
 router.get('/', (req, res) => {
     res.send('Hello Student!')
   })
 
+  router.post('/signup', async (req,res)=>{
+    
+    console.log((req.body)) 
+    res.send('Done').status(200);
+
+  })
 
 module.exports=router
